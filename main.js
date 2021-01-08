1a5b34a0ca4c1b902262a4337ee14204cd5abf62
unknown <jbj1996@gmail.com>
2021-01-08 18:31:19 -0500

    feelsgoodman

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 const fetch = require('node-fetch')
+const express = require('express')
 
-async function main() {
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello ' + req.query.name)
+})
+
+app.get('/issues', async (req, res) => {
+    let issues = await getIssues()
+    res.send(issues)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+async function getIssues() {
 
     let response = await fetch('http://localhost:8000/issues.json')
     .then(R => R.json())
     .catch(E => null)
-
-    console.log(response)
+    return response
 }
-
-main()
