39c364c4b1b1a639ed9554850bbcc4965e1be7be
unknown <jbj1996@gmail.com>
2021-01-09 10:38:49 -0500

    replaced promise function with await

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 const fetch = require('node-fetch')
 const express = require('express')
 const fs = require('fs')
-const { text } = require('express')
 const app = express()
 const port = 3000
 
 app.set('view engine', 'pug')
 
-function readJson(filename) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filename, (err, text) => {
-      if (err) reject('json not found');
-      let data = JSON.parse(text);
-      resolve(data)
-    });
-  })
+async function readJson(filename) {
+  let text = await fs.promises.readFile(filename);
+  let data = JSON.parse(text);
+  return(data)
 }
+// equivalent with promises:
+// function readJson(filename) {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(filename, (err, text) => {
+//       if (err) reject('json not found');
+//       let data = JSON.parse(text);
+//       resolve(data)
+//     });
+//   })
+// }
 
 app.get('/issues', async (req, res) => {
   let issues = await getIssues()
