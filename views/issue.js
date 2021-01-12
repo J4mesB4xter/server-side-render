88d15434d529bd043778301a03aa36b63c9cbeb6
unknown <jbj1996@gmail.com>
2021-01-12 12:34:49 -0500

    refactored issue.js

--- views/issue.js
+++ views/issue.js
@@ -1,14 +1,21 @@
 async function hydrateIssuePage() {
   let issueElement = document.querySelector('.issue');
 
+  hydrateAuthor(issueElement)
+  hydrateDate(issueElement)
+
+}
+
+function hydrateDate(issueElement) {
   let createdAtElement = issueElement.querySelector('.created-at');
   createdAtElement.innerText = formatIso(issueElement.getAttribute('data-created-at'));
-  
+}
+
+async function hydrateAuthor(issueElement) {
   let id = issueElement.getAttribute('data-author');
   let response = await fetch(`http://localhost:8000/contributors/${id}.json`)
     .then(R => R.json())
     .catch(E => null);
-    console.log(response)
   
   let authorElement = issueElement.querySelector('.author');
   authorElement.innerText = response.data.username;
