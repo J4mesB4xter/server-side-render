a40e4900f5fe02943d5e4d4849da0cf46364a8e7
unknown <jbj1996@gmail.com>
2021-01-27 16:50:58 -0500

    began building logout button on navbar

---
+++ views/navbar.js
@@ -0,0 +1,9 @@
+// function loggedIn() {
+//   let token = document.cookie
+//   if (token) {
+//     let loginButton = document.querySelector('[name="comment-body"]');
+//     loginButton.innerText = "Log Out"
+//   }
+//   return
+// }
+// loggedIn()
