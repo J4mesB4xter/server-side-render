e7fb73e7b768098fe78bed5c57f90e84e6a5c9b0
unknown <jbj1996@gmail.com>
2021-02-03 18:59:49 -0500

    logout successfully deletes authentication token on click and toggles button back to login from logout but manually changing the url recreates the cookie which is a significant security issue

--- views/navbar.js
+++ views/navbar.js
@@ -36,8 +36,19 @@ function getCookie(cname) {
   return "";
 }
 
+// var cookies = document.cookie.split(";");
+
+//     for (var i = 0; i < cookies.length; i++) {
+//         var cookie = cookies[i];
+//         var eqPos = cookie.indexOf("=");
+//         var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+//         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+//     }
+// }
+
 function logout() {
-  document.cookie = "authentication-token=; expires=Thur, 01 Jan 1970 00:00:00 UTC; path=/cookies"
+  let name = "authentication-token"
+  document.cookie = name + "=; expires=Thur, 01 Jan 1970 00:00:00 UTC; path=/"
   location.reload()
 }
 
